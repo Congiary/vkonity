7ff05f97b178fa16215bd2753346162e49865469
Andrey Kusmanov <66987756+Charkos0ff@users.noreply.github.com>
2022-12-08 21:14:42 +0700

    Initial commit

---
+++ go.mod
@@ -0,0 +1,18 @@
+module vkonity
+
+go 1.19
+
+require (
+	github.com/BurntSushi/toml v1.2.1
+	github.com/SevereCloud/vksdk/v2 v2.15.0
+	github.com/go-co-op/gocron v1.18.0
+)
+
+require (
+	github.com/klauspost/compress v1.15.8 // indirect
+	github.com/robfig/cron/v3 v3.0.1 // indirect
+	github.com/vmihailenco/msgpack/v5 v5.3.5 // indirect
+	github.com/vmihailenco/tagparser/v2 v2.0.0 // indirect
+	golang.org/x/sync v0.0.0-20210220032951-036812b2e83c // indirect
+	golang.org/x/text v0.3.7 // indirect
+)
---
+++ main.go
@@ -0,0 +1,125 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"github.com/BurntSushi/toml"
+	"github.com/SevereCloud/vksdk/v2/api"
+	"github.com/SevereCloud/vksdk/v2/object"
+	"github.com/go-co-op/gocron"
+	"log"
+	"strings"
+	"time"
+)
+
+type config struct {
+	ServiceToken string
+	MessageToken string
+
+	Admins []int
+	Groups []int
+
+	Interval string
+
+	Message string
+}
+
+var (
+	conf      config
+	counts    [10]int
+	serviceVK *api.VK
+	botVK     *api.VK
+)
+
+func main() {
+	configFile := flag.String("config", "config.toml", "path to the config file")
+	flag.Parse()
+
+	var err error
+	if _, err = toml.DecodeFile(*configFile, &conf); err != nil {
+		log.Fatalln(err)
+	}
+
+	serviceVK = api.NewVK(conf.ServiceToken)
+	botVK = api.NewVK(conf.MessageToken)
+
+	s := gocron.NewScheduler(time.UTC)
+	_, err = s.Every(conf.Interval).Do(check)
+	if err != nil {
+		log.Fatal(err)
+	}
+	s.StartAsync()
+	s.StartBlocking()
+}
+
+func check() {
+	for i, groupId := range conf.Groups {
+		group := get(groupId)
+		count := group.Count
+		previewCount := counts[i]
+
+		counts[i] = count
+		if previewCount == 0 || count <= previewCount {
+			continue
+		}
+
+		post := getPost(group.Items)
+		send(generateMessageText(post), getAttachment(post))
+	}
+}
+
+func getAttachment(post object.WallWallpost) string {
+	attachments := post.Attachments
+	if len(attachments) < 1 {
+		return ""
+	}
+
+	var attachmentsParsed [10]string
+	for i, attachment := range attachments {
+		attachmentsParsed[i] = attachment.Photo.ToAttachment()
+	}
+
+	return strings.Join(attachmentsParsed[:], ",")
+}
+
+func generateMessageText(post object.WallWallpost) string {
+	groupId := post.OwnerID
+	postId := post.ID
+	text := post.Text
+
+	return fmt.Sprintf(conf.Message, -groupId, groupId, postId, text)
+}
+
+func getPost(items []object.WallWallpost) (post object.WallWallpost) {
+	post = items[0]
+
+	if post.IsPinned {
+		post = items[1]
+	}
+
+	return post
+}
+
+func send(text string, attachment string) {
+	_, err := botVK.MessagesSendPeerIDs(api.Params{
+		"user_ids":   conf.Admins,
+		"message":    text,
+		"random_id":  0,
+		"attachment": attachment,
+	})
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func get(groupId int) api.WallGetResponse {
+	group, err := serviceVK.WallGet(api.Params{
+		"count":    2,
+		"owner_id": -groupId,
+	})
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return group
+}
